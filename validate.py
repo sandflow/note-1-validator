97f26987be81a64726d7ed446df7495a72b96544
Pierre-Anthony Lemieux <pal@palemieux.com>
2023-10-23 13:12:04 -0700

    Improve error reporting

--- validate.py
+++ validate.py
@@ -4,6 +4,16 @@ import sys
 from enum import Enum
 import struct
 from itertools import islice
+import argparse
+
+class BadBytesException(Exception):
+  def __init__(self, offset) -> None:
+    self.offset = offset
+    super().__init__(f"0xFFFF detected at offset {offset}")
+
+class CodestreamException(Exception):
+  def __init__(self, msg) -> None:
+    super().__init__(msg)
 
 class Marker(Enum):
   SOT = (0xFF90)
@@ -18,6 +28,11 @@ class Marker(Enum):
   POC = (0xff5f)
   COM = (0xff64)
   TLM = (0xff55)
+  COC = (0xff53)
+  PLT = (0xff58)
+  QCC = (0xff5d)
+  PLM = (0xff57)
+  CRG = (0xff63)
 
   def __new__(cls, marker, is_empty = False):
     obj = object.__new__(cls)
@@ -29,7 +44,7 @@ def read_byte(f, p):
   f.seek(p)
   v = f.read(1)
   if len(v) != 1:
-    raise "Read error in Tile-Part 1"
+    raise "Read error"
   return v[0]
 
 def trigger_positions(main_header_len, tile_part_offset, tile_part_len):
@@ -40,9 +55,9 @@ def trigger_positions(main_header_len, tile_part_offset, tile_part_len):
 def check_tile_part(f, main_header_len, tile_part_offset, tile_part_len):
   for (p1, p2) in trigger_positions(main_header_len, tile_part_offset, tile_part_len):
     if  read_byte(f, p1) == 0xFF and read_byte(f, p2) == 0xFF:
-      raise Exception(f"0xFFFF detected at offset {p1}")
+      raise BadBytesException(p1)
 
-def validate(fn):
+def validate(fn, isVerbose):
   f = open(fn, "rb")
 
   tile_parts_len = []
@@ -72,10 +87,12 @@ def validate(fn):
       f.seek(segment_size - 2, 1)
       continue
 
-    print(f"Found TLM marker segment af position {hex(f.tell() - 2)}")
+    if (isVerbose):
+      print(f"Found TLM marker segment af position {hex(f.tell() - 2)}")
 
     if len(tile_parts_len) > 0:
-      raise "Multiple TLM marker segments"
+      if (isVerbose):
+        raise CodestreamException("Multiple TLM marker segments")
 
     payload = f.read(segment_size - 2)
 
@@ -100,13 +117,16 @@ def validate(fn):
       tile_parts_len.append(entry[-1])
 
   if len(tile_parts_len) != 3:
-    raise "Missing or incomplete TLM marker segment"
+    raise CodestreamException("Missing or incomplete TLM marker segment")
 
-  print(f"Tile-part lengths: {', '.join(map(str, tile_parts_len))}")
+  if isVerbose:
+    print(f"Tile-part lengths: {', '.join(map(str, tile_parts_len))}")
 
   if main_header_len <= 0:
-    raise "Invalid Main Header length"
-  print(f"Main header length: {main_header_len}")
+    raise CodestreamException("Invalid Main Header length")
+
+  if isVerbose:
+    print(f"Main header length: {main_header_len}")
 
   # scan for the forbidden pattern
 
@@ -123,4 +143,13 @@ def validate(fn):
   check_tile_part(f, main_header_len, main_header_len + tile_parts_len[0] + tile_parts_len[1], tile_parts_len[2])
 
 if __name__ == "__main__":
-    validate(sys.argv[1])
+  parser = argparse.ArgumentParser(description="Validate codestream against Compatibility Note 1")
+  parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Print additional information about the codestream")
+  parser.add_argument("codestream", help="Path to the codestream to be validated")
+  args = parser.parse_args()
+
+  try:
+    validate(args.codestream, args.verbose)
+  except Exception as e:
+    sys.stderr.write(f"{e}\n")
+    sys.exit(1)
